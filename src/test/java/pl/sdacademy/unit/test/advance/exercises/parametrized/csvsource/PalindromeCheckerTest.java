c932bea60b17c696687a9f98dc1bbf296a7ce2fc
Pawel Dudko <paweldudko@outlook.com>
2022-05-09 19:58:58 +0200

    solution for ex3 - @CsvFileSource

--- src/test/java/pl/sdacademy/unit/test/advance/exercises/parametrized/csvsource/PalindromeCheckerTest.java
+++ src/test/java/pl/sdacademy/unit/test/advance/exercises/parametrized/csvsource/PalindromeCheckerTest.java
@@ -1,6 +1,7 @@
 package pl.sdacademy.unit.test.advance.exercises.parametrized.csvsource;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,6 +16,16 @@ class PalindromeCheckerTest {
             "zakopane na pokaz, True"
     })
     void shouldVerifyIfStringIsPalindrome(String input, boolean expectedResult) {
+        //when
+        boolean result = PalindromeChecker.isPalindrome(input);
+        //then
+        assertEquals(expectedResult, result); //jUnit
+        assertThat(result).isEqualTo(expectedResult); //asertJ
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/palindrome.csv")
+    void shouldVerifyIfStringIsPalindrome_ByCsvFileSource(String input, boolean expectedResult) {
         //when
         boolean result = PalindromeChecker.isPalindrome(input);
         //then
